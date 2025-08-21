945205e7f243d3e565bed48433f67befc960474b
CarrieForle <53133715+CarrieForle@users.noreply.github.com>
2025-08-21 18:02:44 +0800

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,95 @@
+use chrono::prelude::*;
+use chrono::{NaiveDate, NaiveDateTime, NaiveTime};
+use chrono_tz::Tz;
+use either::Either;
+use std::collections::HashMap;
+use std::path::Path;
+use std::error::Error;
+
+pub fn parse_dt_parts(parts: &[String], tz_abbr: &HashMap<String, String>) -> Result<(NaiveDateTime, Either<Tz, Local>), &'static str> {
+    let now = Local::now();
+    let mut timezone: Option<Tz> = None;
+    let mut date = None;
+    let mut time = None; 
+    let mut pass = false;
+
+    for (i, part) in parts.iter().enumerate() {
+        if pass {
+            pass = false;
+            continue;
+        }
+
+        if let Some(tz) = try_parse_timezone(part, tz_abbr) && let None = timezone {
+            timezone = Some(tz);
+        } else if let Some(d) = try_parse_date(&part) && let None = date {
+            date = Some(d);
+        } else if let None = time {
+            let mut t = None;
+
+            if i + 1 < parts.len() {
+                t = try_parse_time(&format!("{}{}", part, parts[i + 1]));
+            }
+            
+            let p = t.is_some();
+            t = t.or(try_parse_time(part));
+
+            if t.is_some() {
+                time = t;
+                pass = p;
+            } else {
+                Err("Invalid datetime")?;
+            }
+        } else {
+            Err("Invalid datetime")?;
+        }
+    }
+    
+    let date = date.unwrap_or(now.date_naive());
+    let time = time.unwrap_or(now.time());
+    let datetime = NaiveDateTime::new(date, time);
+
+    if let Some(tz) = timezone {
+        Ok((datetime, Either::Left(tz)))
+    } else {
+        Ok((datetime, Either::Right(Local)))
+    }
+}
+
+fn try_parse_timezone(timezone: &str, tz_abbr: &HashMap<String, String>) -> Option<Tz> {
+    Tz::from_str_insensitive(
+        tz_abbr.get(timezone)
+            .map(|x| x.as_str())
+            .unwrap_or(timezone)
+    ).ok()
+}
+
+fn try_parse_time(time: &str) -> Option<NaiveTime> {
+    if time.len() == 3 || time.len() == 4 {
+        return NaiveTime::parse_from_str(&format!("00{time}"), "%M%I%P").ok();
+    }
+
+    NaiveTime::parse_from_str(&time, "%H:%M:%S")
+        .or(NaiveTime::parse_from_str(&time, "%H:%M"))
+        .or(NaiveTime::parse_from_str(&time, "%I:%M:%S%P"))
+        .or(NaiveTime::parse_from_str(&time, "%I:%M%P"))
+        .ok()
+}
+
+fn try_parse_date(date: &str) -> Option<NaiveDate> {
+    NaiveDate::parse_from_str(&date,"%m-%d")
+        .or(NaiveDate::parse_from_str(&date, "%Y-%m-%d"))
+        .ok()
+}
+
+pub fn read_tz_abbr<P: AsRef<Path>>(path: P) -> Result<HashMap<String, String>, Box<dyn Error>> {
+    let contents = std::fs::read_to_string(path)?;
+    let mut map = HashMap::new();
+    const ERROR: &str = "Invalid format";
+
+    for line in contents.lines() {
+        let (key, val) = line.split_once('=').ok_or(ERROR)?;
+        map.insert(key.to_string(), val.to_string());
+    }
+
+    Ok(map)
+}
---
+++ src/main.rs
@@ -0,0 +1,75 @@
+use chrono::Local;
+use chrono::TimeZone;
+use either::Either;
+use tz_rust::*;
+use clap::Parser;
+use clap::CommandFactory;
+use std::error::Error;
+use std::collections::HashMap;
+use std::fmt::Display;
+use clap::ValueEnum;
+
+#[derive(Parser)]
+#[command(name = "tz")]
+#[command(version = "0.1.0")]
+#[command(about = "Timezone converter")]
+pub struct Cli {
+    #[arg(short, long, default_value_t = Mode::To)]
+    pub mode: Mode,
+    #[arg(value_name = "DATETIME_PARTS")]
+    pub dt: Vec<String>,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, ValueEnum)]
+pub enum Mode {
+    From,
+    To,
+}
+
+impl Display for Mode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self {
+            Mode::From => {
+                "from"
+            },
+            Mode::To => {
+                "to"
+            }
+        })
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let tz_abbr = read_tz_abbr("tz.txt").unwrap_or(HashMap::new());
+    let cli = Cli::parse();
+
+    if cli.dt.is_empty() {
+        println!("{}", Cli::command().render_help());
+    } else {
+        let dt = parse_dt_parts(&cli.dt, &tz_abbr);
+        match dt {
+            Ok((dt, Either::Left(tz))) => match cli.mode {
+                Mode::From => {
+                    let dt = tz.from_local_datetime(&dt)
+                        .unwrap()
+                        .with_timezone(&Local);
+                    println!("{}", dt.format("%F %T %s"));
+                }
+                Mode::To => {
+                    let dt = Local.from_local_datetime(&dt)
+                        .unwrap()
+                        .with_timezone(&tz);
+                    println!("{}", dt.format("%F %T %Z %s"));
+                }
+            }
+            Ok((dt, Either::Right(_))) => {
+                println!("{}", dt.format("%F %T %s"));
+            }
+            Err(e) => {
+                Err(e)?;
+            }
+        }
+    }
+
+    Ok(())
+}
